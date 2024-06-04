7ab575cc5c762d8178a274f145c7f423f6d28d72
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-06-04 10:08:01 +1000

    player input and check for a win or draw

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Function to initialize the board
 void initializeBoard(char board[3][3])
 {
     for (int i = 0; i < 3; i++)
@@ -13,7 +12,6 @@ void initializeBoard(char board[3][3])
     }
 }
 
-// Function to display the board
 void displayBoard(const char board[3][3])
 {
     cout << "  0 1 2" << endl;
@@ -30,6 +28,63 @@ void displayBoard(const char board[3][3])
         if (i < 2)
             cout << "  -----" << endl;
     }
+}
+
+// Function to get the player's move
+void getPlayerMove(char board[3][3], char player)
+{
+    int row, col;
+    while (true)
+    {
+        cout << "Player " << player << ", enter your move (row and column): ";
+        cin >> row >> col;
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ')
+        {
+            board[row][col] = player;
+            break;
+        }
+        else
+        {
+            cout << "Invalid move. Try again." << endl;
+        }
+    }
+}
+
+// Function to check for a win
+bool checkWin(const char board[3][3], char player)
+{
+    // Check rows and columns
+    for (int i = 0; i < 3; i++)
+    {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player))
+        {
+            return true;
+        }
+    }
+    // Check diagonals
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player))
+    {
+        return true;
+    }
+    return false;
+}
+
+// Function to check for a draw
+bool checkDraw(const char board[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[i][j] == ' ')
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 int main()
